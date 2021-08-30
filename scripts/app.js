0b8d894f02d23fd57a9d8981b6b439d3a8ff861c
RedaAmelloul <redaamelloul@gmail.com>
2021-08-30 15:06:19 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,31 @@
 // gsap animation
-
 var tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".home",
         start: "top center"
     }
 });
-tl.from("text_home", { duration: .5, y: 30, opacity: 0 })
+tl.from(".text_home", { duration: 1, y: -50, opacity: 0 })
+    .from(".img_home", { duration: 1, y: -50, opacity: 0 })
+
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".about",
+        start: "top center"
+    }
+});
+tl.from(".text_about", { duration: 1, y: 50, opacity: 0 })
+    .from(".img_about", { duration: 1, y: 50, opacity: 0 })
+    // MENU EFFECT
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".team",
+        start: "top center"
+    }
+});
+tl.from(".title_team", { duration: 1, y: -50, opacity: 0 })
+    .from(".box", { duration: 1, y: -50, opacity: 0 })
+    // MENU EFFECT
 
 // MENU EFFECT
 
